230bc179fdac061246b2fd302b78c676faa486af
chase06harmon <chase1harmon@gmail.com>
2025-11-13 02:10:50 -0500

    sparse to dense

--- benchmarks/Connectivity/Gazit1991/Connectivity.h
+++ benchmarks/Connectivity/Gazit1991/Connectivity.h
@@ -12,6 +12,9 @@
 #include <unordered_map>
 #include <utility>
 #include <atomic>
+#include <thread>
+#include <barrier>
+#include <vector>
 
 #include "benchmarks/Connectivity/WorkEfficientSDB14/Connectivity.h"
 #include "benchmarks/Connectivity/common.h"
@@ -104,6 +107,215 @@ inline sequence<Edge> rebuild_edges(sequence<parent>& parents,
   parlay::sort_inplace(edges_slice);
   return parlay::unique(edges_slice);
 }
+
+inline int serial_1(uintE a, uintE b) {
+  uintE diff = a ^ b;
+  return __builtin_ctzll(diff);
+}
+
+inline void deterministic_mate(
+  const sequence<uintE>& V_roots, // vertices currently under consideration (numbers)
+  // const sequence<uint8_t>& active, // is global vertex index active in the active set
+  sequence<parent>& P, // global parent array
+  sequence<int>& next, // global for vertices in v (all else -1)
+  sequence<int>& mate_j, // the round in which v was mated (for later use by partitioning)
+  int k, // the k param
+  int j, // the j round in partitioning
+  int num_threads
+) {
+  size_t n = P.size();
+  size_t n_roots = V_roots.size();
+
+  sequence<uint8_t> removed(n, 0);
+  sequence<uintE> in_deg(n, 0);
+
+  sequence<uintE> prev(n);
+  gbbs::parallel_for(0, n, [&](size_t i) {
+    prev[next[i]] = i;
+  });
+
+  // compute the in-degree from the next array
+  gbbs::parallel_for(0, n_roots, [&](size_t i) {
+    uintE v = V_roots[i];
+    gbbs::fetch_and_add(&in_deg[next[v]], 1);
+  });
+
+
+  gbbs::parallel_for(0, n_roots, [&](size_t i) {
+    uintE v = V_roots[i];
+    
+    if (in_deg[v] == 0) {
+      P[v] = next[v];
+      mate_j[v] = j; 
+      mate_j[next[v]] = j;
+
+      removed[next[v]] = 1; // remove parents of zero degree vertex
+    };
+
+    if (in_deg[v] == 0 || in_deg[v] >= 2) {
+      removed[v] = 1; // remove in_deg 0, 2+
+    }
+  });
+
+  // how to deal with cycles?
+  // for (int i = 0; i < log2(n_roots); i++) {
+  //   gbbs::parallel_for(0, n_roots, [&](size_t i) {
+  //     if (removed[i] || removed[next[i]]) return;
+  //     next[i] = next[next[i]];
+  //   });
+  // }
+
+  // gbbs::parallel_for(0, n_roots, [&](size_t i) {
+  //   if (removed[i]) return;
+
+  //   P[i] = next[i];
+  // });
+
+  // NOTE: From here on out must use removed to make sure v hasn't been removed from the graph
+
+
+  // Can't do pointer jumping (not work efficient)
+
+  // NOTE: pointer jumping probably faster... 
+
+  std::barrier sync_round(num_threads);
+  std::barrier sync_chosen(num_threads);
+
+  std::vector<int> round_taken(n, -1);
+  size_t chunk_size = (n_roots + num_threads - 1) / num_threads; 
+
+  auto worker = [&](int tid) {
+    int lo = tid * chunk_size; 
+    int hi = std::min(n_roots, lo+chunk_size);
+
+    int cur_pos = lo;
+    int round = 0;
+
+    while (round < chunk_size + 2*log2(log2(n))) {
+      if (cur_pos >= hi) { // no more vertices to process
+        sync_chosen.arrive_and_wait();
+        sync_round.arrive_and_wait();
+        round++;
+        continue;
+      }
+
+      if (removed[V_roots[cur_pos]]) { // if the vertex has been removed, we don't need to consider it
+        cur_pos++;
+        continue;
+      }
+
+      uintE v = V_roots[cur_pos];
+      round_taken[v] = round;
+
+      // if (tid == 1) {
+      //   std::cout << "v: " << v << std::endl;
+      // }
+
+      sync_chosen.arrive_and_wait();
+
+      if (round_taken[next[v]] == round) { // SERIAL calculation and work backwards
+        uintE next_v = next[v]; 
+        uintE prev_v = prev[v];
+        uint8_t is_tail = 0; 
+
+        if (next[next_v] < 0 || round_taken[next[next_v]] != round) {
+          is_tail = 1;
+
+        } else if (prev_v < 0 || round_taken[prev_v] != round) {
+          is_tail = 1;
+
+        } else {
+          int my_val = serial_1(v, next_v);
+          int prev_val = serial_1(prev_v, v);
+          int next_val = serial_1(next_v, next[next_v]);
+
+          if ((my_val > prev_val) || (my_val == prev_val && (v & (1u << my_val))) && (my_val > next_val) || (my_val == next_val && (v & (1u << my_val)))) {
+            is_tail = 1;
+          } else {
+            is_tail = 0;
+          }
+        }
+
+        if (is_tail) {
+          int freeze_round = round;
+
+          sync_round.arrive_and_wait();
+          round++;
+          
+          uintE cur_v = v;
+          
+          while (1) {
+            cur_v = prev[cur_v];
+
+            uintE next_v = next[cur_v]; 
+            uintE prev_v = prev[cur_v];
+            uint8_t is_tail = 0;
+
+            if (prev_v < 0 || round_taken[prev_v] != freeze_round) {
+              break;
+            }
+
+            int my_val = serial_1(cur_v, next_v);
+            int prev_val = serial_1(prev_v, cur_v);
+            int next_val = serial_1(next_v, next[next_v]);
+
+            if ((my_val > prev_val) || (my_val == prev_val && (cur_v & (1u << my_val))) && (my_val > next_val) || (my_val == next_val && (cur_v & (1u << my_val)))) {
+              break;
+            }
+
+            sync_chosen.arrive_and_wait();
+
+            P[cur_v] = v;
+            sync_round.arrive_and_wait();
+            round++;
+          }
+        } else {
+          sync_round.arrive_and_wait();
+          round++;
+        }
+
+      } else {
+        P[v] = P[next[v]];
+        removed[next[v]] = 1;
+        cur_pos++;
+        sync_round.arrive_and_wait();
+        round++;
+      }
+    }
+  };
+
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+  for (int t = 0; t < num_threads; ++t) {
+      threads.emplace_back(worker, t);
+  }
+  for (auto& th : threads) th.join();
+
+}
+
+inline std::pair<sequence<uintE>, sequence<uintE>> partitioning(
+  size_t n,
+  sequence<uintE>& V, 
+  sequence<Edge>& E
+) {
+  // double alpha = 0.5;
+
+  // sequence<uintE> P(n);
+  // sequence<uint32_t> flag(n, 0);
+  // sequence
+
+  // gbbs::parallel_for(0, n, [&](size_t i) {P[i] = static_cast<parent>(i);});
+
+  // for (int j = 0; j < ceil(2*log(log(n))); j++) {
+  //   double p  = E.size() * pow(alpha, j);
+  //   uint64_t seed = j;
+
+  //   auto keep = gbbs::filter(gbbs::iota(E.size()))
+  // }
+}
+
+
+
 
 inline sequence<parent> easy_case(size_t n, const sequence<Edge>& edges) {
   sequence<parent> P(n), Scratch(n);
@@ -204,6 +416,70 @@ inline sequence<parent> easy_case(size_t n, const sequence<Edge>& edges) {
 
   return P;
 }
+
+/*
+Summary of High-Level Steps
+
+Start with a sparse graph.
+
+Iteratively partition it into dense and sparse subsets.
+
+Sample edges to find dense (“extrovert”) vertices.
+
+Merge extroverts into supervertices via deterministic mating.
+
+Replace edges to connect only supervertex roots.
+
+Repeat until graph size ≤ 
+𝑛
+/
+log
+⁡
+𝑛
+n/logn.
+
+Proceed with dense-to-easy reduction and then the easy-case algorithm.
+*/
+
+}
+
+void test_deterministic_mate() {
+  size_t N = 100000;
+
+  sequence<uintE> V_roots(N);
+  sequence<uintE> P(N);
+  sequence<int> next(N, -1);
+  sequence<int> mate_j(N, -1);
+  int k = 0; 
+  int j = 0; 
+  int num_threads = N / log2(N);
+
+  gbbs::parallel_for(0, N, [&](size_t i){V_roots[i] = i; P[i] = i; next[i] = (i+1) % N;});
+
+
+  // size_t N = 9;
+
+  // sequence<uintE> V_roots(N);
+  // sequence<uintE> P(N);
+  // sequence<int> mate_j(N, -1);
+  // int k = 0; 
+  // int j = 0; 
+  // int num_threads = N / log2(N);
+
+  // gbbs::parallel_for(0, N, [&](size_t i){V_roots[i] = i; P[i] = i;});
+
+  // sequence<int> next = {1, 2, 1, 4, 2, 3, 7, 8, 4};
+
+  internal::deterministic_mate(V_roots, P, next, mate_j, 0, 0, num_threads);
+
+  std::cout << "Num threads: " << num_threads << std::endl;
+  std::cout << "Per thread: " << (N + num_threads - 1) / num_threads  << std::endl;
+
+  // for (int i = 0; i < N; i++) {
+  //   // if (P[i] == P[i+1]) {
+  //     std::cout << "i: " << i << " p: " << P[i] << std::endl;
+  //   // }
+  // }
 
 }
 
---
+++ benchmarks/Connectivity/Gazit1991/Sparse2Dense.cc
@@ -0,0 +1,14 @@
+#include "benchmarks/Connectivity/Gazit1991/Connectivity.h"
+
+// namespace gbbs {
+// namespace gazit_cc {
+
+// }  // namespace gazit_cc
+// }  // namespace gbbs
+
+
+int main(int argc, char* argv[]) {
+    gbbs::gazit_cc::test_deterministic_mate();
+    return 1;
+}
+
