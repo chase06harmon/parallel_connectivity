dde36bc290b66cbd7b75f70b08efed48d26e8f36
chase06harmon <chase1harmon@gmail.com>
2025-12-07 23:32:38 -0500

    scripts

--- scripts/gazit_test_run.py
+++ scripts/gazit_test_run.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":
         "--timeout",
         "-t",
         type=float,
-        default=10000000000,
+        default=6000,
         help="(seconds) - Halt benchmarks that run longer than this time.",
     )
 
